59656d56dd3937bcd2bb8d275a9352b0827d6ce5
Charlie Paik <chpaik88@gmail.com>
2021-12-03 07:42:10 -0800

    imported react into index.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+  return (
+    <>
+      <div id='Title'>Healthful</div>
+    </>
+  )
+};
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import React from 'react';
 import App from './components/App.jsx';
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
