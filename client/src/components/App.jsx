b11af7b3982c0b57470d5c8b3c09f7df7526ec59
Charlie Paik <chpaik88@gmail.com>
2021-12-04 00:59:08 -0800

    added calendar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import MealList from './MealList.jsx';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const App = () => {
   const LB_TO_CALORIES = 3500;
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + 7);
   const [mealData, setMealData] = useState(null);
   const [currentWeight, setCurrentWeight] = useState(0);
   const [goalWeight, setGoalWeight] = useState(0);
+  const [byDate, setByDate] = useState(new Date());
+  const [targetDays, setTargetDays] = useState(0);
+  const [minDays, setMinDays] = useState(0);
+
+  useEffect( () => {
+    const minCalories = 1200;
+    const caloriesTolose = (currentWeight - goalWeight) * LB_TO_CALORIES;
+    const minDaysToLoseSafely = Math.ceil(caloriesTolose / minCalories);
+    setMinDays(minDaysToLoseSafely);
+  }, [currentWeight, goalWeight])
 
   const getMealData = (calories) => {
     axios.get(`/meals/${calories}`)
@@ -27,8 +41,6 @@ const App = () => {
   }
 
   const initMealPlan = () => {
-    console.log('current weight', currentWeight);
-    console.log('goal weight', goalWeight);
     var dailyAllowedCalories = getDailyCalories();
     if ( dailyAllowedCalories < 800 ) {
       alert('Danger: daily caloric intake too low');
@@ -46,8 +58,28 @@ const App = () => {
     } else {
       alert('Please enter your current weight and goal weight');
     }
-    dailyCalories = 2000 - (totalCaloriesToLose / 90);
+    dailyCalories = 2000 - (totalCaloriesToLose / targetDays);
     return dailyCalories;
+  }
+
+  const onDateChange = (newDate) => {
+    var day = 24 * 60 * 60 * 1000;
+    var numDays = (Math.ceil((newDate - byDate) / day));
+    setTargetDays(numDays);
+  }
+
+  const displayCalendar = () => {
+    var minDate = new Date();
+    minDate.setDate(minDate.getDate() + minDays);
+    return (
+      <Calendar
+        onChange={onDateChange}
+        value={byDate}
+        showNeighboringMonth={true}
+        locale={'en-US'}
+        minDate={minDate}
+      />
+    )
   }
 
   return (
@@ -57,6 +89,14 @@ const App = () => {
         <section className='goals'>
           <input type='number' placeholder='Current Weight' onChange={handleCurrentWeightChange} />
           <input type='number' placeholder='Goal Weight' onChange={handleGoalWeightChange} />
+          {minDays > 0 && displayCalendar()}
+          {/* <Calendar
+            onChange={onDateChange}
+            value={byDate}
+            showNeighboringMonth={true}
+            locale={'en-US'}
+            minDate={currentDate}
+          /> */}
         </section>
         <button onClick={initMealPlan}>Get Meals</button>
       </div>
