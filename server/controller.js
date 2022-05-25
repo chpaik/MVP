b126babfd13fd749580870170805529b11ab97bb
Charlie Paik <chpaik88@gmail.com>
2022-05-24 23:11:19 -0700

    test token

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,11 @@
-const {TOKEN} = require('../config.js');
+// const {TOKEN} = require('../config.js');
 const axios = require('axios');
 
 const controller = {
   getMeals: (req, res) => {
     console.log('request', req.params);
     const calories = req.params.calories;
-    axios.get(`https://api.spoonacular.com/mealplanner/generate?apiKey=${TOKEN}&timeFrame=day&targetCalories=${calories}`)
+    axios.get(`https://api.spoonacular.com/mealplanner/generate?apiKey=6a1d9533b91b439086d7787de7cee37c&timeFrame=day&targetCalories=${calories}`)
       .then( response => {
         console.log('response', response.data);
         res.status(200).send(response.data);
@@ -18,7 +18,7 @@ const controller = {
 
   getRecipe: (req, res) => {
     const mealID = req.params.mealID;
-    axios.get(`https://api.spoonacular.com/recipes/${mealID}/information?apiKey=${TOKEN}&includeNutrition=false`)
+    axios.get(`https://api.spoonacular.com/recipes/${mealID}/information?apiKey=6a1d9533b91b439086d7787de7cee37c&includeNutrition=false`)
       .then( response => {
         res.status(200).send(response.data);
       })
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const morgan = require('morgan');
 const cors = require('cors');
 const router = require('./routes.js');
